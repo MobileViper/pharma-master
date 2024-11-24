fc0709798627149804d0b3d09e3e59d6e88d9e08
MobileViper <codmobileviper1000@gmail.com>
2024-11-24 19:15:56 +0300

    Update server.js to use dynamic port for Heroku

--- server.js
+++ server.js
@@ -30,9 +30,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
 
 // User schema and model
 const userSchema = new mongoose.Schema({
